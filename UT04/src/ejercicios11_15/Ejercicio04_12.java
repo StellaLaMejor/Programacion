8b20dcab1f7a6c19df0d8c497f9e1fe37a3d4e8c
StellaLaMejor <146403819+StellaLaMejor@users.noreply.github.com>
2023-10-13 18:50:53 +0200

    a

--- UT04/src/ejercicios11_15/Ejercicio04_12.java
+++ UT04/src/ejercicios11_15/Ejercicio04_12.java
@@ -7,13 +7,47 @@ public class Ejercicio04_12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner sc= new Scanner (System.in);
-		
-		System.out.println("Tipos de uva: A y B \nTamaños de uva: 1 y 2 \n");
-		System.out.print("Indica el tipo de uva: ");
-		String tipo= sc.nextLine();
-		System.out.print("Indica el tamaño de uva: ");
-		
-	}
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Indica el precio inicial por kilo de uva: ");
+		double inicial = sc.nextDouble();
+		sc.nextLine();
+		System.out.println("Indica el tipo de uva (A o B): ");
+		String tipo = sc.nextLine();
+		System.out.println("Indica el tamaño de uva (1 o 2): ");
+		int tamanio = sc.nextInt();
+
+		if (!tipo.equalsIgnoreCase("A") && !tipo.equalsIgnoreCase("B")) {
+			System.out.println("Tipo de uva incorrecto. Debe ser A o B.");
+		} else if (tamanio != 1 && tamanio != 2) {
+			System.out.println("Número de tamaño incorrecto. Debe ser 1 o 2.");
+		} else {
+			if (tipo.equalsIgnoreCase("A")) {
+			} else {
+				if (tamanio == 1) {
+					double a1 = (inicial + 0.2);
+					System.out.printf("El precio por kilo de uva es: %.2f", a1);
+				} else {
+					if (tamanio == 2) {
+						double a2 = (inicial + 0.3);
+						System.out.printf("El precio por kilo de uva es: %.2f", a2);
+					}
+				}
+			}
+			if (tipo.equalsIgnoreCase("B")) {
+			} else {
+				if (tamanio == 1) {
+					double b1 = (inicial - 0.3);
+					System.out.printf("El precio por kilo de uva es: %.2f", b1);
+				} else {
+					if (tamanio == 2) {
+						double b2 = (inicial - 0.5);
+						System.out.printf("El precio por kilo de uva es: %.2f", b2);
+					}
+
+				}
+			}
+		}
+
+	}
 }
