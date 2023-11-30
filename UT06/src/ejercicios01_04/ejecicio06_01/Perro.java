c79fec67ac157be925b088dfc92a555f23e625f4
Stella <stellafpb@gmail.com>
2023-11-30 20:39:37 +0100

    aaaa

--- UT06/src/ejercicios01_04/ejecicio06_01/Perro.java
+++ UT06/src/ejercicios01_04/ejecicio06_01/Perro.java
@@ -2,4 +2,5 @@ package ejercicios01_04.ejecicio06_01;
 
 public class Perro {
 
+
 }
--- UT06/src/ejercicios01_04/ejecicio06_01/Programa.java
+++ UT06/src/ejercicios01_04/ejecicio06_01/Programa.java
@@ -3,7 +3,25 @@ package ejercicios01_04.ejecicio06_01;
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+//		Crea un paquete para el ejercicio. En los ejercicios de clases necesitamos paquetes porque siempre crearemos al menos dos clases.
+//		Crea una clase Perro que tenga los siguientes atributos:
+//		•	Nombre del perro
+//		•	Raza
+//		•	Nombre del propietario.
+//		•	Edad (años)
+//		•	Peso en kg (puede tener decimales)
+//		Crea getters y setters para todos los atributos.
+//		Crea ahora una clase “Programa”, con el programa principal que:
+//		•	Tenga un método “mostrarPerro”. Este método recibe un objeto de la clase Perro y muestra en consola sus atributos. Para hacerlo, tendrás 
+//			que acceder a ellos a través de los getters. Debe mostrar un atributo por línea
+//			
+//		•	Tenga un método main en el que:
+//			o	Cree un objeto de la clase Perro.
+//			o	Llame al método mostrarPerro, para que se muestren los atributos con su valor por defecto.
+//			Fíjate en qué valores tienen los atributos. Serán sus valores por defecto cada vez que se creen objetos Perro.
+
 
 	}
 
