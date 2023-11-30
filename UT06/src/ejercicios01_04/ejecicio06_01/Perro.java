3d9e22169215b1f0ffd728e18420a491f634e24a
Stella <stellafpb@gmail.com>
2023-11-30 19:04:47 +0100

    a

---
+++ UT06/src/ejercicios01_04/ejecicio06_01/Perro.java
@@ -0,0 +1,5 @@
+package ejercicios01_04.ejecicio06_01;
+
+public class Perro {
+
+}
---
+++ UT06/src/ejercicios01_04/ejecicio06_01/Programa.java
@@ -0,0 +1,10 @@
+package ejercicios01_04.ejecicio06_01;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
