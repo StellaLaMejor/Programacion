881b9bbd440a7a453d109cda278f606150eadf61
Stella <stellafpb@gmail.com>
2023-12-01 20:43:43 +0100

    a

--- UT06/src/ejercicios01_04/ejecicio06_01/Programa.java
+++ UT06/src/ejercicios01_04/ejecicio06_01/Programa.java
@@ -15,14 +15,43 @@ public class Programa {
 //		Crea getters y setters para todos los atributos.
 //		Crea ahora una clase “Programa”, con el programa principal que:
 //		•	Tenga un método “mostrarPerro”. Este método recibe un objeto de la clase Perro y muestra en consola sus atributos. Para hacerlo, tendrás 
-//			que acceder a ellos a través de los getters. Debe mostrar un atributo por línea
-//			
+//			que acceder a ellos a través de los getters. Debe mostrar un atributo por líneaperrito
 //		•	Tenga un método main en el que:
 //			o	Cree un objeto de la clase Perro.
 //			o	Llame al método mostrarPerro, para que se muestren los atributos con su valor por defecto.
 //			Fíjate en qué valores tienen los atributos. Serán sus valores por defecto cada vez que se creen objetos Perro.
 
+		
+		Perro perrito1 = new Perro ();
+
+		System.out.println(mostrarPerro(perrito1));
 
 	}
+	
+	
+	private static String mostrarPerro (Perro perrito)
+	{
 
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("El nombre del perro es: ");
+		sb.append(perrito.getNombrePerro()+ "\n");
+		
+		sb.append("La raza del perro es: ");
+		sb.append(perrito.getRaza()+ "\n");
+		
+		sb.append("El nombre del propietario es: ");
+		sb.append(perrito.getNombreDueño()+ "\n");
+		
+		sb.append("La edad del perro es: ");
+		sb.append(perrito.getEdad()+ "\n");
+		
+		sb.append("El peso del perro es: ");
+		sb.append(perrito.getPeso()+ "\n");
+
+		String cadenaCompleta= sb.toString();
+
+		return cadenaCompleta;
+
+	}
 }
