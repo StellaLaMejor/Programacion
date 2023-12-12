258768f3b902a77f08668c6aeb26bfc61b4a50cd
Stella <stellafpb@gmail.com>
2023-12-12 20:00:26 +0100

    A

--- UT06/src/ejercicios06_06/Persona.java
+++ UT06/src/ejercicios06_06/Persona.java
@@ -1,19 +1,34 @@
 package ejercicios06_06;
 
+import java.util.Random;
+
 public class Persona {
+	
+	Random rdm = new Random ();
+	
+
 	
 	private static final double PESO_DEFECTO = 0;
 	private static final int ALTURA_DEFECTO = 0;
 	private static final int EDAD_DEFECTO = 0;
 	private static final Sexo SEXO_DEFECTO = Sexo.M;
 	
+	private static final int DEBAJO_PESO_IDEAL= -1;
+	private static final int PESO_IDEAL= 0;
+	private static final int ENCIMA_PESO_IDEAL= 1;
+
+	
+	
 	private String nombre;
 	private int edad;
-	private String dni;
+	private String dni= generaDNI();
 	private Sexo sexo;
 	private double peso;
 	private int altura;
 	
+	public Persona() {
+		
+	}
 	public Persona(String nombre, String dni) 
 	{
 		this(nombre, EDAD_DEFECTO, dni, SEXO_DEFECTO);
@@ -85,6 +100,86 @@ public class Persona {
 		this.altura = altura;
 	}
 	
+	
+	
+	private int calcularIMC ()
+	{
+		double alturaMetros= this.altura/100;
+		double pesoIdeal= this.peso/(Math.pow(alturaMetros, 2));
+		
+		if (pesoIdeal<20)
+		{
+			return DEBAJO_PESO_IDEAL;
+		}
+		if(pesoIdeal>=20 && pesoIdeal<=25)
+		{
+			return PESO_IDEAL;
+		}
+		else 
+		{
+		return ENCIMA_PESO_IDEAL;	
+		}
+	}
+	
+	public void mensajeIMC ()
+	{
+		 int resultadoIMC = calcularIMC();
+
+		if (resultadoIMC== DEBAJO_PESO_IDEAL)
+		{
+			System.out.printf("El peso indicado (%.2f) está por debajo del peso ideal.\n",this.peso );
+		}
+		else if (resultadoIMC==PESO_IDEAL)
+		{
+			System.out.printf("El peso indicado (%.2f) está en el peso ideal.\n",this.peso );
+		}
+		else 
+		{
+			System.out.printf("El peso indicado (%.2f) está por encima del peso ideal.\n",this.peso );
+		}
+	}
+	
+	
+	public boolean esMayorDeEdad()
+	{
+		if (this.edad >= 18)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	
+	private String generaDNI() 
+	{
+		int numeroRdm= rdm.nextInt(0, 100_000_000);
+		char letraRdm= (char) rdm.nextInt((int)'A', (int) 'Z');
+		
+		return String.format("%08d%s", numeroRdm, letraRdm);
+		
+	}
+	
+	
+	
+	
+	public String toString() 
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append("Persona [nombre=");
+		builder.append(nombre);
+		builder.append(", edad=");
+		builder.append(edad);
+		builder.append(", DNI=");
+		builder.append(dni);
+		builder.append(", sexo=");
+		builder.append(sexo);
+		builder.append(", peso=");
+		builder.append(peso);
+		builder.append(", altura=");
+		builder.append(altura);
+		builder.append("]");
+		return builder.toString();
+	}
 	
 
 }
--- UT06/src/ejercicios06_06/ProgramaPersona.java
+++ UT06/src/ejercicios06_06/ProgramaPersona.java
@@ -1,8 +1,57 @@
 package ejercicios06_06;
 
+import java.util.Scanner;
+
 public class ProgramaPersona {
 
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner (System.in);
+		Persona p1 = new Persona();
+		
+		System.out.print("Introduce el nombre: ");
+		p1.setNombre(sc.nextLine());
+		System.out.print("Introduce la edad: ");
+		p1.setEdad(sc.nextInt());
+		System.out.print("Introduce el sexo (M/H): ");
+		p1.setSexo(Sexo.valueOf(sc.next()));
+		System.out.print("Introduce el peso: ");
+		p1.setPeso(sc.nextDouble());
+		System.out.print("Introduce la altura: ");
+		p1.setAltura(sc.nextInt());
+
+		
+		System.out.print("¿Es mayor de edad? "+ p1.esMayorDeEdad()+ "\n");
+		p1.mensajeIMC();
+		
+		System.out.println(p1.toString());
+
+		System.out.println();
+		
+		Persona p2 = new Persona ("Stella La Mejor", 20, "877857437A", Sexo.H);
+		
+		System.out.print("¿Es mayor de edad? "+ p2.esMayorDeEdad()+ "\n");
+		p2.mensajeIMC();
+		
+		System.out.println(p2.toString());
+		
+		System.out.println();
+
+		
+		Persona p3 = new Persona();
+		
+		p3.setAltura(130);
+		p3.setDni("834958L");
+		p3.setEdad(13);
+		p3.setNombre("Stella Mola");
+		p3.setPeso(25);
+		p3.setSexo(Sexo.M);
+
+		
+		System.out.print("¿Es mayor de edad? "+ p3.esMayorDeEdad()+ "\n");
+		p3.mensajeIMC();
+		
+		System.out.println(p3.toString());
 
 
 	}
