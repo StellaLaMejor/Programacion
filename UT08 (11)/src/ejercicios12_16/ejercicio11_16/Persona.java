4e3f69c675bcc0c510d8588d2c76be86bf0a50c5
Stella <stellafpb@gmail.com>
2024-01-26 19:00:14 +0100

    a

--- UT08 (11)/src/ejercicios12_16/ejercicio11_16/Persona.java
+++ UT08 (11)/src/ejercicios12_16/ejercicio11_16/Persona.java
@@ -1,28 +1,36 @@
 package ejercicios12_16.ejercicio11_16;
 
-public class Persona implements Comparable{
-	
+public class Persona implements Comparable {
+
 	private String nombre;
 	private String apellido;
 	
-	public Persona(String nombre ,String apellido) {
-		
-		this.nombre= nombre;
-		this.apellido = apellido;
-		
-	}
-	
-	
-	@Override
-	public String toString() {
-		return String.format("%s %s",nombre, apellido); 
+	public String getApellido() {
+		return apellido;
 	}
 
+	public Persona(String nombre, String apellido) {
+
+		this.nombre = nombre;
+		this.apellido = apellido;
+
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s %s", nombre, apellido);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
 
 	@Override
 	public int compareTo(Object o) {
-		return 0;
+		
+		return this.apellido.compareTo(((Persona) o).getApellido());
 	}
-	
-	
+
 }
